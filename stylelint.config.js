78da0d9f285a07d73c46fad38932ebcc937f95f1
ill_Nino <innxfect@gmail.com>
2025-07-08 14:58:02 +0300

    refactor(project): update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,7 @@
 export default {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-clean-order'],
+  rules: {
+    'scss/dollar-variable-pattern': null,
+    'custom-property-pattern': null,
+  },
 };
