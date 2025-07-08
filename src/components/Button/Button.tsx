7a7993a0746f41a013c740d876f3b91b90e95661
ill_Nino <innxfect@gmail.com>
2025-07-08 14:56:59 +0300

    feat(project): add project structure

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import styles from './Button.module.scss';
+
+const ButtonElement = () => {
+  return <div className={styles.button}>Button</div>;
+};
+
+export default ButtonElement;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,7 @@
+import styles from './Input.module.scss';
+
+const InputElement = () => {
+  return <div className={styles.input}>Input</div>;
+};
+
+export default InputElement;
