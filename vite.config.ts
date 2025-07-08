c77d315a2cec084df1dadf8005b509e4a4226e53
ill_Nino <innxfect@gmail.com>
2025-07-08 12:58:28 +0300

    feat(project): add dependencies

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      {
+        find: '@assets',
+        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
+      {
+        find: '@components',
+        replacement: fileURLToPath(
+          new URL('./src/components', import.meta.url)
+        ),
+      },
+    ],
+  },
+});
