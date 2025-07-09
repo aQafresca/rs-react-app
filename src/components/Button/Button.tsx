09e8cdf7bb916106439a20118db9cefacfc7d582
ill_Nino <innxfect@gmail.com>
2025-07-09 12:23:04 +0300

    feat(components): add button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,32 @@
 import styles from './Button.module.scss';
+import type { ButtonHTMLAttributes, FC, JSX } from 'react';
+import clsx from 'clsx';
 
-const ButtonElement = () => {
-  return <div className={styles.button}>Button</div>;
+type TButtonVariant = 'primary' | 'secondary';
+type TButtonSize = 'x-small' | 'small' | 'medium' | 'large';
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: TButtonVariant;
+  size?: TButtonSize;
+}
+
+const ButtonElement: FC<IButtonProps> = ({
+  className,
+  children,
+  variant = 'primary',
+  size = 'medium',
+  type = 'button',
+  ...rest
+}: IButtonProps): JSX.Element => {
+  return (
+    <button
+      type={type}
+      className={clsx(styles.button, styles[variant], styles[size], className)}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default ButtonElement;
