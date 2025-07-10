528a1f37ea099cf9fd36813f772a5632ceff63bf
ill_Nino <innxfect@gmail.com>
2025-07-10 11:51:13 +0300

    feat(project): add Loader in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-function App() {
+import Header from '@components/Header/Header.tsx';
+import Loader from '@components/Loader/Loader.tsx';
+import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary.tsx';
+import { Suspense } from 'react';
+import type { JSX } from 'react';
+import CardList from '@components/CardList/CardList.tsx';
+
+function App(): JSX.Element {
   return (
-    <>
-      <div>aaa</div>
-    </>
+    <ErrorBoundary>
+      <>
+        <Header />
+        <main className="container">
+          <Suspense fallback={<Loader />}>
+            <CardList />
+          </Suspense>
+        </main>
+      </>
+    </ErrorBoundary>
   );
 }
 
