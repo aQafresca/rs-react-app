44eaa627086f10f556f5aa38ebd9641ed263582c
ill_Nino <innxfect@gmail.com>
2025-07-11 14:25:32 +0300

    feat(components): add search char logic in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,44 @@
 import { Component, type JSX } from 'react';
+import * as React from 'react';
 import styles from './Header.module.scss';
 import Button from '@components/Button/Button.tsx';
 import InputElement from '@components/Input/Input.tsx';
 import { FaSearch } from 'react-icons/fa';
 import BrokenComponent from '@components/BrokenComponent/BrokenComponent.tsx';
-import { BUTTON_LABELS, PLACEHOLDERS } from '@/constants/constants.ts';
+import {
+  BUTTON_LABELS,
+  PLACEHOLDERS,
+  LOCALSTORAGE_KEYS,
+} from '@/constants/constants.ts';
+import { searchStore } from '@/core/store/serchStore.ts';
 
 class Header extends Component {
-  state: { showErrorComponent: boolean } = {
+  state: { showErrorComponent: boolean; inputValue: string } = {
     showErrorComponent: false,
+    inputValue: '',
+  };
+
+  componentDidMount(): void {
+    const savedValue: string | null = localStorage.getItem(
+      LOCALSTORAGE_KEYS.SEARCH_QUERY
+    );
+
+    console.log(savedValue);
+
+    if (savedValue) {
+      this.setState({ inputValue: savedValue });
+      searchStore.setQuery(savedValue);
+    }
+  }
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const trimmedValue: string = event.target.value.trim();
+    this.setState({ inputValue: trimmedValue });
+  };
+
+  handleSearchClick = (): void => {
+    searchStore.setQuery(this.state.inputValue);
+    localStorage.setItem(LOCALSTORAGE_KEYS.SEARCH_QUERY, this.state.inputValue);
   };
 
   handleErrorClick = (): void => {
@@ -18,8 +48,12 @@ class Header extends Component {
   render(): JSX.Element {
     return (
       <header className={`container ${styles.header}`}>
-        <InputElement placeholder={PLACEHOLDERS.SEARCH} />
-        <Button>
+        <InputElement
+          placeholder={PLACEHOLDERS.SEARCH}
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        />
+        <Button onClick={this.handleSearchClick}>
           <FaSearch />
         </Button>
         <Button onClick={this.handleErrorClick}>{BUTTON_LABELS.ERROR}</Button>
