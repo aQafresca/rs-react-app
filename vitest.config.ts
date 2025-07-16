71d7215582f033725c36d5f0cf72d7aefd7a4985
ill_Nino <innxfect@gmail.com>
2025-07-16 18:04:48 +0300

    refactor(project): move test.cfg from vite.config.ts to vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/main.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      {
+        find: '@assets',
+        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
+      {
+        find: '@components',
+        replacement: fileURLToPath(
+          new URL('./src/components', import.meta.url)
+        ),
+      },
+    ],
+  },
+});
