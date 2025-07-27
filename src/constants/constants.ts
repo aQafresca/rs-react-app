5db4060be0cb8e817f92c2541e263d0af01e72c4
Siarhei Buiko <innxfect@gmail.com>
2025-07-27 19:44:16 +0300

    refactor(constants): update constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,8 @@
 export const BUTTON_LABELS = {
   ERROR: 'Error',
   RELOAD: 'Reload',
+  CLOSE: 'Close',
+  EXIT: 'X',
 };
 
 export const PLACEHOLDERS = {
@@ -11,15 +13,38 @@ export const CHAR = {
   STATUS: 'Status:',
   SPECIES: 'Species:',
   GENDER: 'Gender:',
+  ORIGIN: 'Origin:',
+  LOCATION: 'Location:',
 };
 
 export const BASE_LIMIT_VISIBLE_PAGINATION = 3;
 export const ELLIPSIS = '.';
-export const API_URL = 'https://rickandmortyapi.com/api/character?page=';
+
 export const EMPTY_LIST = {
   TEXT: 'Please enter a different Char name and click "Search"',
 };
 
+export const API_URL = {
+  PAGE: 'https://rickandmortyapi.com/api/character?page=',
+  ID: 'https://rickandmortyapi.com/api/character/',
+};
+
 export const LOCALSTORAGE_KEYS = {
   SEARCH_QUERY: 'search_query',
+};
+
+export const ROUTES = {
+  HOME: '/',
+  ABOUT: '/about',
+  CHARACTERS: '/characters',
+  NOT_FOUND: '*',
+};
+
+export const menuLinks: { route: string; caption: string }[] = [
+  { route: ROUTES.HOME, caption: 'Home' },
+  { route: ROUTES.ABOUT, caption: 'About' },
+];
+
+export const NOT_FOUND = {
+  ERROR: 'error 404',
 };
