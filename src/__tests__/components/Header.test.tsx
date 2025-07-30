1495a04c823bd62d5a2ad15e8d31ab8b16b3d190
Siarhei Buiko <innxfect@gmail.com>
2025-07-30 17:05:34 +0300

    feat(tests): update header component test

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,14 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect, beforeEach } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, vi, expect, beforeEach } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 import Header from '@/components/Header/Header.tsx';
 import { menuLinks } from '@/constants/constants.ts';
+import { ThemeProvider } from '@/context/theme/ThemeProvider.tsx';
+
+vi.mock('@components/Button/Button.tsx', () => ({
+  default: ({
+    onClick,
+    children,
+  }: {
+    onClick: () => void;
+    children: React.ReactNode;
+  }) => (
+    <div role="button" onClick={onClick}>
+      {children}
+    </div>
+  ),
+}));
 
 describe('Header Component', () => {
   beforeEach(() => {
     render(
       <BrowserRouter>
-        <Header />
+        <ThemeProvider>
+          <Header />
+        </ThemeProvider>
       </BrowserRouter>
     );
   });
@@ -24,5 +41,21 @@ describe('Header Component', () => {
 
   it('renders a navigation element', (): void => {
     expect(screen.getByRole('navigation')).toBeInTheDocument();
+  });
+
+  it('renders the theme toggle icon inside button', (): void => {
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button.innerHTML).not.toBe(''); // проверка, что иконка есть
+  });
+
+  it('toggles theme when button is clicked', (): void => {
+    const button = screen.getByRole('button');
+    const initialClass = document.documentElement.className;
+
+    fireEvent.click(button);
+
+    const newClass = document.documentElement.className;
+    expect(newClass).not.toBe(initialClass);
   });
 });
