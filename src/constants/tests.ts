cf88eac8cf4676ff7ec30724e2d9ef09397e29b8
Siarhei Buiko <innxfect@gmail.com>
2025-08-03 13:41:18 +0300

    feat(constants): add new constants

--- src/constants/tests.ts
+++ src/constants/tests.ts
@@ -1,3 +1,5 @@
+import type { TCharacter } from '@/shema/characterShema.ts';
+
 export const mockData = {
   info: {
     count: 1,
@@ -26,3 +28,20 @@ export const mockData = {
 };
 
 export const character = mockData.results[0];
+
+export const baseCharacter: TCharacter = {
+  id: 1,
+  name: 'Rick Sanchez',
+  status: 'Alive',
+  species: 'Human',
+  gender: 'Male',
+  image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+  origin: {
+    name: 'Citadel of Ricks',
+    url: 'https://rickandmortyapi.com/api/location/3',
+  },
+  location: {
+    name: 'Earth (C-137)',
+    url: 'https://rickandmortyapi.com/api/location/1',
+  },
+};
