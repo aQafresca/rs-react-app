ff07b804c71aecfff3bbbb2a043eb740f8971a90
Siarhei Buiko <innxfect@gmail.com>
2025-08-18 00:17:21 +0300

    refactor(app):  change a/Link

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,8 @@
 import styles from './about.module.scss';
 import { ABOUT_ME } from '@/constants/texts.ts';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
+
 const AboutPage = () => {
   const t = useTranslations('AboutPage');
 
@@ -8,14 +10,14 @@ const AboutPage = () => {
     <div className={styles.about}>
       <p className={styles.about__text}>
         {t('text.part1')}
-        <a
+        <Link
           className={styles.about__link}
           target="_blank"
           href="https://rs.school/courses/reactjs"
           rel="noopener noreferrer"
         >
           {ABOUT_ME.PART_2}
-        </a>
+        </Link>
         {t('text.part2')}
       </p>
     </div>
