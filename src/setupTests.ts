6d649359c2c2f2bf404957e0831dcbf724d2f8ee
Siarhei Buiko <innxfect@gmail.com>
2025-08-17 23:45:13 +0300

    feat(tests): add setup/config

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,6 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
-afterEach((): void => {
+afterEach(() => {
   cleanup();
 });
