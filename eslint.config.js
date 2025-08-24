02e34f4ef45a1c24671402dc95ac1ef953bcf8ab
Siarhei Buiko <innxfect@gmail.com>
2025-08-24 23:27:54 +0300

    refactor: update project config

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ export default tseslint.config([
       import: importPlugin,
     },
     rules: {
-      'no-console': ['error', { allow: ['warn', 'error'] }],
+      // 'no-console': ['error', { allow: ['warn', 'error'] }],
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
       'padding-line-between-statements': [
